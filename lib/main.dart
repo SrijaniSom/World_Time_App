9ae04169e6a4d1fc42b13043ca697bfa1fed9a7b
Srijani Som <81528176+SrijaniSom@users.noreply.github.com>
2022-05-30 12:36:11 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,14 @@ import 'Pages/home_screen.dart';
 import 'Pages/choose_location.dart';
 import 'Pages/loading.dart';
 
-void main() => runApp(MaterialApp(
-    initialRoute: '/home',
+void main() async => runApp(MaterialApp(
+  
+    
     routes: {
        '/': (context) => Loading(),
      '/home': (context) => Home(),
      '/choose_location':(context) => ChooseLocation(),
     },
+    initialRoute: '/',
     
     ));
